348d14c36071cdabec07239564feabc6800ea52b
GenZonix-Lab <solutiongenzonix@gmail.com>
2025-06-20 16:43:01 +0530

    add storage trail 1

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,87 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { storage } from './storage/resource';
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
   auth,
   data,
-  storage
 });
 
 const { cfnUserPool } = backend.auth.resources.cfnResources
 // an empty array denotes "email" and "phone_number" cannot be used as a username
 cfnUserPool.usernameAttributes = []
+
+
+const customBucketStack = backend.createStack("custom-bucket-stack");
+
+
+// Import existing bucket
+const customBucket = Bucket.fromBucketAttributes(customBucketStack, "MyCustomBucket", {
+  bucketArn: "arn:aws:s3:::amplify-d1nncltkg8y8mu-ma-imagestoragebucket11771a-19sdlpppi0jo",
+  region: "ap-south-1"
+});
+
+
+backend.addOutput({
+  storage: {
+    aws_region: customBucket.env.region,
+    bucket_name: customBucket.bucketName,
+    // optional: `buckets` can be used when setting up more than one existing bucket
+    buckets: [
+      {
+        aws_region: customBucket.env.region,
+        bucket_name: customBucket.bucketName,
+        name: customBucket.bucketName,
+        /*
+          optional: `paths` can be used to set up access to specific 
+          bucket prefixes and configure user access types to them
+        */ 
+        paths: {
+          "/*": {
+            // "write" and "delete" can also be added depending on your use case
+            guest: ["get", "list"], 
+          },
+        },
+      }
+    ]
+  },
+});
+
+
+/*
+  Define an inline policy to attach to Amplify's unauth role
+  This policy defines how unauthenticated/guest users can access your existing bucket
+*/ 
+const unauthPolicy = new Policy(backend.stack, "customBucketUnauthPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject"],
+      resources: [`${customBucket.bucketArn}/public/*`],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:ListBucket"],
+      resources: [
+        `${customBucket.bucketArn}`,
+        `${customBucket.bucketArn}/*`
+      ],
+      conditions: {
+        StringLike: {
+          "s3:prefix": ["/", "/*"],
+        },
+      },
+    }),
+  ],
+});
+
+
+// Add the policies to the unauthenticated user role
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  unauthPolicy,
+);
