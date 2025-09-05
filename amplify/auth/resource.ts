6146fd614c777084904aadd8af21f447f486cca5
GenZonix-Lab <solutiongenzonix@gmail.com>
2025-09-05 06:29:22 +0530

    oauth google authenticator correction

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,81 +6,41 @@ import { defineAuth, secret } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      callbackUrls: [
-        'https://genzonix.in/profile'
+    externalProviders:{
+      google: {
+        clientId: secret("gid"),
+        clientSecret: secret('gst'),
+        scopes:["openid", "profile", "email"],
+        attributeMapping:{
+          email:"email",
+          fullname:"name",
+        }
+      },
+      logoutUrls:[
+        "https://genzonix.in"
       ],
-      logoutUrls: ['https://genzonix.in'],
+      callbackUrls:[
+        "https://genzonix.in/profile"
+      ],
     }
   },
-    
-  userAttributes: {
-    fullname: {
-      mutable: true,
-      required: true,
-    },
-    phoneNumber: {
-      mutable: true,
-      required: false,
-    },    
-    "custom:doorNo": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 300,
-    },
-    address: {
-      mutable: true,
-      required: false,
-    },
-    "custom:addressLine2": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 300,
-    },
-    "custom:addressLine3": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 300,
-    },
-    "custom:districtCode": {
-      dataType: "String",
-      mutable: true,
-    },
-    "custom:district": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 100,
-    },
-    "custom:stateCode": {
-      dataType: "String",
-      mutable: true,
-    },
-    "custom:states": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 100,
-    },
-    "custom:countryCode": {
-      dataType: "String",
-      mutable: true,
-    },
-    "custom:country": {
-      dataType: "String",
-      mutable: true,
-      minLen: 1,
-      maxLen: 100,
-    },
-    "custom:pincode": {
-      dataType: "String",
-      mutable: true,
-      minLen: 5,
-      maxLen: 7,
-    },
 
+
+  userAttributes: {
+    fullname: {mutable: true,required: true,},
+    phoneNumber: {mutable: true,required: false,},
+    "custom:doorNo": {dataType: "String",mutable: true,minLen: 1,maxLen: 300,},
+    address: {mutable: true,required: false,},
+    "custom:addressLine2": {dataType: "String",mutable: true,minLen: 1,maxLen: 300,},
+    "custom:addressLine3": {dataType: "String",mutable: true,minLen: 1,maxLen: 300,},
+    "custom:districtCode": {dataType: "String",mutable: true,},
+    "custom:district": {dataType: "String",mutable: true,minLen: 1,maxLen: 100,},
+    "custom:stateCode": {dataType: "String",mutable: true,},
+    "custom:states": {dataType: "String",mutable: true,minLen: 1,maxLen: 100,},
+    "custom:countryCode": {dataType: "String",mutable: true,},
+    "custom:country": {dataType: "String",mutable: true,minLen: 1,maxLen: 100,},
+    "custom:pincode": {dataType: "String",mutable: true,minLen: 5,maxLen: 7,},
   }
 });
+
+console.log("Auth configuration:", auth);
